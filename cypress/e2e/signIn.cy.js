44262c7fffa0c5b6a9cbf6af3fe3cae39077b7c9
bhashana99 <bhashanachamodya99@gmail.com>
2024-10-28 16:26:52 +0530

    check navigate the home page

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -8,6 +8,8 @@ describe("Success Test Login Suite", () => {
   it("TC_OTT_Login_001", () => {
     cy.get("#username").type("hiranga@vclhq.com");
     cy.get("#password").type("!!!Woofy123");
-    cy.get('#kc-login').click()
+    cy.get('#kc-login').click();
+
+    cy.url().should("eq","https://clarke-admanager-stg.testlogdia.lk/")
   });
 });
