01466399566c68074013b3bf8a1c3bb9fbc1a279
bhashana99 <bhashanachamodya99@gmail.com>
2024-10-28 16:00:43 +0530

    add beforeEach hook for before run each test navigate the Ott page

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
-describe("Success Test Login Suite", ()=>{
-
-
-})
+describe("Success Test Login Suite", () => {
+  beforeEach(() => {
+    cy.visit("https://clarke-admanager-stg.testlogdia.lk");
+  });
+});
