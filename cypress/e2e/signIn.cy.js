366344ac39e8e5cef823f2254cec793e5360475d
bhashana99 <bhashanachamodya99@gmail.com>
2024-10-29 09:47:03 +0530

    add comment each test cases

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,25 +1,25 @@
-
-
 describe("Success Test Login Suite", () => {
   beforeEach(() => {
     cy.visit("https://clarke-admanager-stg.testlogdia.lk");
   });
 
+  // Enter valid username & Password
   it("TC_OTT_Login_001", () => {
     cy.get("#username").type("hiranga@vclhq.com");
     cy.get("#password").type("!!!Woofy123");
-    cy.get('#kc-login').click();
+    cy.get("#kc-login").click();
 
-    cy.url().should("eq","https://clarke-admanager-stg.testlogdia.lk/")
+    cy.url().should("eq", "https://clarke-admanager-stg.testlogdia.lk/");
   });
 
-  it("TC_OTT_Login_002", ()=>{
+  // Enter valid username & invalid Password
+  it("TC_OTT_Login_002", () => {
     cy.get("#username").type("hiranga@vclhq.com");
     cy.get("#password").type("!!!Woofy12345");
-    cy.get('#kc-login').click();
+    cy.get("#kc-login").click();
 
-    cy.get('#input-error').should('be.visible').contains("Invalid username or password.")
-    
-  })
-
+    cy.get("#input-error")
+      .should("be.visible")
+      .contains("Invalid username or password.");
+  });
 });
