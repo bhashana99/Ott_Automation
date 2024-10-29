26241aef985bf9640629c9c3f5d3a068355bdab5
bhashana99 <bhashanachamodya99@gmail.com>
2024-10-29 13:17:57 +0530

    define beforeEach function to navigate to NewDisplayCreative page

---
+++ cypress/e2e/displayCreative.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe("Display Creative Happy path", () => {
+  //Login
+  beforeEach(() => {
+    cy.visit("https://clarke-admanager-stg.testlogdia.lk");
+    cy.get("#username").type("hiranga@vclhq.com");
+    cy.get("#password").type("!!!Woofy123");
+    cy.get("#kc-login").click();
+    cy.url().should("eq", "https://clarke-admanager-stg.testlogdia.lk/");
+
+    cy.xpath('//*[@id="root"]/div/nav/div/div/ul/div[2]/div[2]/p')
+      .should("contain", "Delivery")
+      .click();
+    cy.xpath(
+      '//*[@id="root"]/div/nav/div/div/ul/div[3]/div/div/ul/a[1]'
+    ).click();
+    cy.url().should(
+      "eq",
+      "https://clarke-admanager-stg.testlogdia.lk/delivery/creative"
+    );
+
+    cy.xpath(
+      '//*[@id="simple-tabpanel-0"]/div/div/div/div[1]/button[1]'
+    ).click();
+    cy.url().should(
+      "eq",
+      "https://clarke-admanager-stg.testlogdia.lk/NewDisplayCreative"
+    );
+  });
+
+});
