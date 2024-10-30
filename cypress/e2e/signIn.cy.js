7282b192497636997841c7eacec9573017c56c84
bhashana99 <bhashanachamodya99@gmail.com>
2024-10-30 14:17:10 +0530

    update code using Adding command and fixture

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,71 +1,82 @@
+
+let data;
+
+before(()=>{
+  cy.fixture('loginData.json').then((loginData) => {
+    data = loginData;
+  })
+})
+
+beforeEach(() => {
+  cy.loginPageNavigate()
+});
+
 describe("Success Test Login Suite", () => {
-  beforeEach(() => {
-    cy.visit("https://clarke-admanager-stg.testlogdia.lk");
-  });
+ 
 
   // Enter valid username & Password
   it("TC_OTT_Login_001", () => {
-    cy.get("#username").type("hiranga@vclhq.com");
-    cy.get("#password").type("!!!Woofy123");
+    cy.get("#username").type(data.validUsername);
+    cy.get("#password").type(data.validPassword);
     cy.get("#kc-login").click();
 
-    cy.url().should("eq", "https://clarke-admanager-stg.testlogdia.lk/");
+    cy.url().should("eq", data.navigateUrl);
   });
 
   // Enter valid username & invalid Password
   it("TC_OTT_Login_002", () => {
-    cy.get("#username").type("hiranga@vclhq.com");
-    cy.get("#password").type("!!!Woofy12345");
+    cy.get("#username").type(data.validUsername);
+    cy.get("#password").type(data.invalidPassword);
     cy.get("#kc-login").click();
 
     cy.get("#input-error")
       .should("be.visible")
-      .contains("Invalid username or password.");
+      .contains(data.errorMessage);
   });
 
   // Enter invalid username & valid Password
   it("TC_OTT_Login_003", () => {
-    cy.get("#username").type("hiranga1@vclhq.com");
-    cy.get("#password").type("!!!Woofy123");
+    cy.get("#username").type(data.invalidUsername);
+    cy.get("#password").type(data.validPassword);
     cy.get("#kc-login").click();
 
     cy.get("#input-error")
       .should("be.visible")
-      .contains("Invalid username or password.");
+      .contains(data.errorMessage);
   });
 
   // Enter invalid username & invalid Password
   it("TC_OTT_Login_004", () => {
-    cy.get("#username").type("hiranga1@vclhq.com");
-    cy.get("#password").type("!!!Woofy12345");
+    cy.get("#username").type(data.invalidUsername);
+    cy.get("#password").type(data.invalidPassword);
     cy.get("#kc-login").click();
 
     cy.get("#input-error")
       .should("be.visible")
-      .contains("Invalid username or password.");
+      .contains(data.errorMessage);
   });
 
   //Leave username field blank & enter valid password
   it("TC_OTT_Login_005", () => {
     cy.get("#username").should("be.empty");
-    cy.get("#password").type("!!!Woofy123");
+    cy.get("#password").type(data.validPassword);
     cy.get("#kc-login").click();
 
     cy.get("#input-error")
       .should("be.visible")
-      .contains("Invalid username or password.");
+      .contains(data.errorMessage);
   });
 
   // Enter valid username & Leave password field blank
   it("TC_OTT_Login_006", () => {
-    cy.get("#username").type("hiranga@vclhq.com");
+    cy.get("#username").type(data.validUsername);
     cy.get("#password").should("be.empty");
 
     cy.get("#kc-login").click();
 
     cy.get("#input-error")
       .should("be.visible")
-      .contains("Invalid username or password.");
+      .contains(data.errorMessage);
   });
 
   // Leave username field & password field blank
@@ -77,32 +88,30 @@ describe("Success Test Login Suite", () => {
 
     cy.get("#input-error")
       .should("be.visible")
-      .contains("Invalid username or password.");
+      .contains(data.errorMessage);
   });
 
   //Enter invalid username & Leave password field blank
   it("TC_OTT_Login_008", () => {
-    cy.get("#username").type("hiranga1@vclhq.com");
+    cy.get("#username").type(data.invalidUsername);
     cy.get("#password").should("be.empty");
 
     cy.get("#kc-login").click();
 
     cy.get("#input-error")
       .should("be.visible")
-      .contains("Invalid username or password.");
+      .contains(data.errorMessage);
   });
 
   // Leave username field blank & enter invalid password
   it("TC_OTT_Login_009", () => {
     cy.get("#username").should("be.empty");
-    cy.get("#password").type("!!!Woofy12345");
+    cy.get("#password").type(data.invalidPassword);
     cy.get("#kc-login").click();
 
     cy.get("#input-error")
       .should("be.visible")
-      .contains("Invalid username or password.");
+      .contains(data.errorMessage);
+
   });
-
-
-
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("loginPageNavigate", () => {
+  cy.visit("https://clarke-admanager-stg.testlogdia.lk");
+});
