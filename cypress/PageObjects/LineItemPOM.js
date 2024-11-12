8c15fb081df89e548701eba5a127e54dcf6b8cb0
bhashana99 <bhashanachamodya99@gmail.com>
2024-11-12 10:15:48 +0530

    method for choose First Url From Table

--- cypress/PageObjects/LineItemPOM.js
+++ cypress/PageObjects/LineItemPOM.js
@@ -1,22 +1,39 @@
-class LineItemPOM{
-    clickVideoIconForCreateVideoLineItem(){
-        cy.xpath('//*[@id="root"]/div/main/div/div/div[1]')
-        .click()
-    }
+class LineItemPOM {
+  clickVideoIconForCreateVideoLineItem() {
+    cy.xpath('//*[@id="root"]/div/main/div/div/div[1]').click();
+  }
 
-    validateIsLineItemPage(){
-        cy.xpath('//*[@id="root"]/div/main/div/div/h5')
-        .should('contain','New Line Item')
-    }
-    setLineItemName(lineItemName){
-        cy.xpath('//*[@id="root"]/div/main/div/div/div[1]/div[1]/div/div')
-        .type(lineItemName)
-    }
+  validateIsLineItemPage() {
+    cy.xpath('//*[@id="root"]/div/main/div/div/h5').should(
+      "contain",
+      "New Line Item"
+    );
+  }
+  setLineItemName(lineItemName) {
+    cy.xpath('//*[@id="root"]/div/main/div/div/div[1]/div[1]/div/div').type(
+      lineItemName
+    );
+  }
 
-    setTrafficker(trafficker){
-        cy.xpath('//*[@id="root"]/div/main/div/div/div[1]/div[2]/div/div')
-        .type(trafficker)
-    }
+  setTrafficker(trafficker) {
+    cy.xpath('//*[@id="root"]/div/main/div/div/div[1]/div[2]/div/div').type(
+      trafficker
+    );
+  }
+
+  chooseFirstUrlFromTable() {
+    cy.xpath(
+      '//*[@id="root"]/div/main/div/div/div[2]/div/div/table/tbody/tr/td[8]/button'
+    ).click();
+
+    // cy.xpath(
+    //   '//*[@id="root"]/div/main/div/div/div[2]/div/div/table/tbody/tr/td[3]'
+    // )
+    //   .invoke("text")
+    //   .then((tableUrl) => {
+    //     cy.log(`URL: ${tableUrl}`);
+    //   });
+  }
 }
 
 export default LineItemPOM;
